0feed0b80d0dddc248c74c413f15c57acb8aecb1
FodoBM <fodo.bm@gmail.com>
2018-04-29 00:38:33 -0500

    1st upload

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Pokedex AI' });
+});
+
+router.get('/name', function(req, res) {
+    res.render("name");
+});
+
+router.post('/process_post', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.body.first_name,
+      last_name:req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+});
+
+module.exports = router;
