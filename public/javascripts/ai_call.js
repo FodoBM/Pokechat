5c70bc56e6cb1a913c0ce005fafaff8473f3a8a8
FodoBM <fodo.bm@gmail.com>
2018-05-04 15:10:30 -0500

    Ya habla mi bot

--- public/javascripts/ai_call.js
+++ public/javascripts/ai_call.js
@@ -1,6 +1,6 @@
 var call = (data_input) => {
   var question = data_input;
-  console.log(question);
+  //console.log(question);
   var session_id = '&session_id=1';
   var key = '&key=47c621deff7e47c30ab1cabd32c5de7237d94827';
   var call_url = 'http://beta.soldai.com/bill-cipher/askquestion?';
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var jQuery = require('jquery');
+var axios = require('axios');
 var router = express.Router();
 var call_to = require('../public/javascripts/ai_call.js');
 
@@ -13,13 +14,20 @@ router.get('/about', function(req, res) {
 });
 
 router.get('/pokechat', function(req, res) {
-    if(Object.keys(req.query).length != 0 ){
-      var temp = req.query.input_call;
-      console.log(temp);
-      var call_from = call_to(temp);
-      console.log(call_from);
-      res.end();//esto es temporal apra que no se recargue la solicitud.
-    }else{
+  if(Object.keys(req.query).length != 0 ){
+    var temp = req.query.input_call;
+    //console.log(temp);
+    var call_from = call_to(temp);
+    //console.log(call_from);
+    axios.get(call_from)
+    .then(response => {
+      console.log(response.data.current_response.resolvedQuery);
+      console.log(response.data.current_response.message);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }else{
     res.render("pokechat", {title: 'Pokechat'});
   }
 });
