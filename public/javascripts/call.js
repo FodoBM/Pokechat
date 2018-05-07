96ea1dcf50255ec10fd55a58a76e709ce4a59ac8
FodoBM <fodo.bm@gmail.com>
2018-05-07 03:35:42 -0500

    Backend terminado, falta mejorar el frontend

--- public/javascripts/call.js
+++ public/javascripts/call.js
@@ -1,1 +1,43 @@
+function write_answer(str){
+  document.getElementById('messages').innerHTML +=
+  '<div(class="msg-answer")>'+
+  '<div class="arrow-left"></div>'+
+  '<div class="content">'+
+  '<p>'+str+'</p>'+
+  '</div></div>';
+}
+function write_response(str){
+  document.getElementById('messages').innerHTML +=
+  '<div(class="msg-question")>'+
+  '<div class="arrow-right"></div>'+
+  '<div class="content">'+
+  '<p>'+str+'</p>'+
+  '</div></div>';
+}
 
+function read(){
+  return document.getElementById('input_call').value;
+}
+function call(data_input) {
+  var question = data_input;
+  var session_id = '&session_id=1';
+  var key = '&key=47c621deff7e47c30ab1cabd32c5de7237d94827';
+  var call_url = 'http://beta.soldai.com/bill-cipher/askquestion?';
+  var log = '&log=1';
+  var url = call_url+'question='+question+session_id+key+log;
+  return url;
+}
+
+function call_my(){
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){ if( this.readyState == 4 && this.status == 200){
+    var data = JSON.parse(this.responseText);
+    write_response(data.current_response.message);
+  //  write( this.responseText);
+  }}
+
+  xhttp.open('GET',call(read()),true);
+  xhttp.send();
+  write_answer(read());
+  document.getElementById('input_call').value = '';
+ }
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var jQuery = require('jquery');
 var axios = require('axios');
 var router = express.Router();
 var call_to = require('../public/javascripts/ai_call.js');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -14,34 +15,11 @@ router.get('/about', function(req, res) {
 });
 
 router.get('/pokechat', function(req, res) {
-  if(Object.keys(req.query).length != 0 ){
-    var temp = req.query.input_call;
-    //console.log(temp);
-    var call_from = call_to(temp);
-    //console.log(call_from);
-    axios.get(call_from)
-    .then(response => {
-      console.log(response.data.current_response.resolvedQuery);
-      console.log(response.data.current_response.message);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  }else{
     res.render("pokechat", {title: 'Pokechat'});
-  }
 });
 
 router.get('/contact', function(req, res) {
     res.render("name", {title: 'Contact'});
 });
 
-router.get('/process_post', function (req, res) {
-   // Prepare output in JSON format
-   http.get(req.body, function(req,res) {
-     response ={ resp:req.body.defautl_response};
-     console.log(response);
-     res.end(JSON.stringify(response));
-   });
-});
 module.exports = router;
